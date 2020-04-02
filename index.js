a26753793d315211f075f2db57ece422f654ec41
kalmegati <kalmegati@gmail.com>
2020-04-02 19:35:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    document.body.querySelector("p").innerHTML="This is really cool!"
+});
