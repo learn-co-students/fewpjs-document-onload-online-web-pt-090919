27be0988ea353f53f42cf84646828b7becc144af
fasnimo <fasnimo@gmail.com>
2020-04-03 22:51:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+
+let event = document.addEventListener('DOMContentLoaded', function() {
+    let newContent = document.querySelector('p');
+    newContent.innerHTML = `This is really cool!`;
+    console.log(newContent);
+   
+});
+
+    let content = document.querySelector('p');
+    content.innerHTML = `JavaScript is so cool. It lets me add text to my page programmatically.`;
+    console.log(content);
